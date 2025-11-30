087b10c9981d15ee6ea68ee9c5cc4a59e6992702
Emmanuel Olarewaju <emmanuelmark501@gmail.com>
2025-11-30 22:39:23 +0100

    changing classname to classlist.add

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Create remove button
     const removeBtn = document.createElement("button");
     removeBtn.textContent = "Remove";
-    removeBtn.className = "remove-btn";
+    removeBtn.classList.add = "remove-btn";
 
     // Remove task when clicked
     removeBtn.onclick = function () {
